435ba8bb48e97e31ad4d77c37633dfece3b233b7
wndgml13 <wndgml13>
2022-08-30 01:16:52 +0900

    no message

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,10 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {
-  RecoilRoot,
-  atom,
-  selector,
-  useRecoilState,
-  useRecoilValue,
-} from 'recoil';
+
 
 function App() {
   return (
-    <RecoilRoot>
     <h1 className="text-3xl font-bold underline">
       Hello world!
       <div className="h-14 bg-gradient-to-r from-cyan-500 to-blue-500"></div>
@@ -19,7 +12,6 @@ function App() {
       <div className="h-14 bg-gradient-to-r from-violet-500 to-fuchsia-500"></div>
       <div className="h-14 bg-gradient-to-r from-purple-500 to-pink-500"></div>
     </h1>
-    </RecoilRoot>
   );
 }
 
