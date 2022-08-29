3cbc1621b67502d9f1e7dd8d48b0ef1b784e29f4
wndgml13 <wndgml13>
2022-08-29 14:01:09 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+  RecoilRoot,
+  atom,
+  selector,
+  useRecoilState,
+  useRecoilValue,
+} from 'recoil';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RecoilRoot>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+      <div className="h-14 bg-gradient-to-r from-cyan-500 to-blue-500"></div>
+      <div className="h-14 bg-gradient-to-r from-sky-500 to-indigo-500"></div>
+      <div className="h-14 bg-gradient-to-r from-violet-500 to-fuchsia-500"></div>
+      <div className="h-14 bg-gradient-to-r from-purple-500 to-pink-500"></div>
+    </h1>
+    </RecoilRoot>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,27 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { QueryClient, QueryClientProvider } from "react-query";
+import {
+  RecoilRoot,
+  atom,
+  selector,
+  useRecoilState,
+  useRecoilValue,
+} from 'recoil';
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <QueryClientProvider client={queryClient}>
+    <RecoilRoot>
+      <App />
+    </RecoilRoot>
+    {/* devtools 넣기 */}
+  </QueryClientProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
